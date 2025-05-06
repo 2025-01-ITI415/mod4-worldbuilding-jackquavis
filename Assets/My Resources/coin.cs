16ec31f07fd88cfd302db90731cecbac7dc65709
jackquavis <125089721+jackquavis@users.noreply.github.com>
2025-05-06 14:08:51 -0400

    Much added
    Added final doorway and collectible coins. also added lighting

---
+++ Assets/My Resources/coin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coin : MonoBehaviour{
+    public AudioClip collectSound;
+    private AudioSource audioSource;
+    private Rigidbody rb;
+	int coinCount;
+
+    void Start () {
+	    rb = GetComponent<Rigidbody>();
+        coinCount = 0;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)  {
+        if (other.gameObject.CompareTag ("Coin")) {
+            other.transform.root.gameObject.SetActive(false);
+            //other.gameObject.SetActive (false);
+            coinCount +=1;
+            if (collectSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(collectSound);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
